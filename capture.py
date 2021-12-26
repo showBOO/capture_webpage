12e21abc88a45b51d07ab2e652f8baa1896fe2f5
showBOO <k.showboo@gmail.com>
2021-12-26 14:23:51 +0900

    first commit

--- capture.py
+++ capture.py
@@ -1,20 +1,46 @@
+import time
+from selenium import webdriver
 
 
 
 
 
+def main():
 
+    # Webdriver用オプション
+    options = webdriver.ChromeOptions()
 
+    # headless
+    options.add_argument('--headless')
+    options.add_argument("--disable-hang-monitor")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
 
+    driver = webdriver.Chrome("C:\\webdriver\\chromedriver.exe", options=options)
 
+    with open('pages.txt') as f:
+        pages = f.readlines()
+        
+    i=0
+    
+    for page in pages:
 
+        # ページにアクセス
+        driver.get(page)
+        print(page)
+        time.sleep(3)
+        #w = driver.execute_script("return document.body.scrollWidth;")
+        h = driver.execute_script("return document.body.scrollHeight;")
+        driver.set_window_size(1920,h)
+        driver.save_screenshot('savefiles/screenshot'+str(i)+'.png')
 
+        # ブラウザを閉じる
+        
+        i +=1
 
+    driver.quit()
 
-
-
-
-
-
-if __name__ == "__main__":
-
+if __name__ == '__main__':
+    main()
